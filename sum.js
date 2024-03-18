a9bdd105b33b063f8d14ebd2f4ece92187d7ffd0
Cecilia Belón <cecilia.belon@hotmail.com>
2024-03-18 14:56:31 -0300

    upd: refactorización de clase Calculator

--- sum.js
+++ sum.js
@@ -8,37 +8,70 @@ TODO:
 */
 class Calculator { 
 	calculator() {
-		this.ans = "";
-		this.ansString = "";
+		this.ans = 0;
 		this.result = 0;
 	}
-	/*
-	//string of last answer
-	this.ansString = "";
-	//numeric value of last answer
-	this.ans = "";
-	//numeric
-	this.result = 0;
 	
-	const sumString = "+";
-	const subString = "-";
-	const multString = "*";
-	const divString = "/";
-	*/
 	clearMemory() {
-		this.ans = "";
+		this.ans = 0;
+		this.result = 0;
 	}
 	
+	//estas operaciones son entre el result anterior y el nuevo valor
+	mult(c) {
+		this.saveAns(this.ans + " * " + c, "*");
+		this.result = this.result * c;
+		console.log(this.ans + " = " + this.result);
+	}
+	
+	div(c) {
+		this.saveAns(this.result + " / " + c, "/");
+		this.result = this.result / c;
+		console.log(this.ans + " = " + this.result);
+	}
+	
+	sub(c) {
+		this.saveAns(this.result + " - " + c, "-");
+		this.result = this.result - c;
+		console.log(this.ans + " = " + this.result);
+	}
+	
+	sm(c) {
+		this.saveAns(this.result + " + " + c, "+");
+		this.result = this.result + c;
+		console.log(this.ans + " = " + this.result);
+	}
+
+	//estas operaciones son entre los dos valores sólamente
 	multiply(a, b) {
-		this.saveAns(a + " * " + b, "*");
 		this.result = a * b;
-		console.log(this.ans + " = " + (a*b));
+		this.saveAns(a + " * " + b, "*");
+		console.log(a + " * " + b + " = " + this.result);
+	}
+	
+	divide(a, b) {
+		this.result = a / b;
+		this.saveAns(a + " / " + b, "/");
+		console.log(a + " / " + b + " = " + this.result);
+	}
+	
+	substract(a, b) {
+		this.result = a - b;
+		this.saveAns(a + " - " + b, "-");
+		console.log(this.ans +  " = " + this.result);
+	}
+	
+	sum(a, b) {
+		this.result = a + b;
+		this.saveAns(a + " + " + b, "+");
+		console.log(this.ans +  " = " + this.result);
 	}
 	
 	saveAns(operationValue, operation) {
-		let operands = operationValue.split(operation);
+		let operands = operationValue.split(/[+\-*\/]/g);
+		//console.log(operands);
 		if(operands.length > 2) {
-			this.ans = "Ans";
+			this.ans = operationValue;
 		} else {
 			this.ans = operationValue;
 		}
@@ -46,9 +79,23 @@ class Calculator {
 	}
 }
 
+//creación del objeto calculator
 const calculator = new Calculator();
-
-calculator.multiply(1,2);
+calculator.clearMemory();
+calculator.mult(2);
+//operaciones entre 2 valores
+console.log("multiplicar, dividir, restar y sumar con dos valores");
+calculator.multiply(2,2);
+calculator.divide(8,4);
+calculator.substract(10,3);
+calculator.sum(1,2);
+//operaciones entre result y un nuevo valor
+console.log("multiplicar, dividir, restar y sumar con un valor solo");
+calculator.mult(2);
+calculator.div(2);
+calculator.clearMemory();
+calculator.sub(1);
+calculator.sm(3);
 
 
 function sum(a, b) {
