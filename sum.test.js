df04a7222b851c535245e6347d162a630945eed9
Cecilia Belón <cecilia.belon@hotmail.com>
2024-04-11 14:12:08 -0300

    feat: jest tests

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,29 @@
 const sum = require('./sum');
 
+beforeAll(() => {
+  sum = new sum();
+});
+
+afterAll(() => {
+  sum = new sum();
+});
+
+beforeEach(() => {
+  console.log("beforeEach: result is " + sum.result);
+});
+
+afterEach(() => {
+  console.log("afterEach: result is " + sum.result);
+});
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('substracts 2 - 1 to equal 1', () => {
+  expect(substract(1, 2)).toBe(1);
+});
+
+test('multiplies 1 * 2 to equal 1', () => {
+  expect(mult(2)).toBe(2);
 });
